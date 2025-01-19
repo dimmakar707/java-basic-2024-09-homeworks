97266e56f33d651b3a179f167060592d074f59a4
Dmitriy Makarenkov <sitesun707@gmail.com>
2025-01-19 16:27:17 +0300

    Домашнее задание 19 выполнено

---
+++ src/main/java/ru/otus/java/basic/homeworks/homework19/Box.java
@@ -0,0 +1,34 @@
+package ru.otus.java.basic.homeworks.homework19;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Box<T extends Fruit> {
+    private List<T> fruits = new ArrayList<>();
+
+    public void add(T fruit) {
+        fruits.add(fruit);
+    }
+
+    public double weight() {
+        double weight = 0.0;
+        for (int i = 0; i < fruits.size(); i++) {
+            weight += fruits.get(i).getWeight();
+        }
+        return weight;
+    }
+
+    public boolean compare(Box anotherBox) {
+        return Math.abs(this.weight() - anotherBox.weight()) < 0.0001;
+    }
+
+    public void moveFruitsToAnotherBox(Box<? super T> anotherBox) {
+        Iterator<T> iterator = fruits.iterator();
+        while(iterator.hasNext()) {
+            T fruit = iterator.next();
+            anotherBox.add(fruit);
+            iterator.remove();
+        }
+    }
+}
---
+++ src/main/java/ru/otus/java/basic/homeworks/homework19/Orange.java
@@ -0,0 +1,7 @@
+package ru.otus.java.basic.homeworks.homework19;
+
+public class Orange extends Fruit {
+    public Orange(double weight) {
+        super(weight);
+    }
+}

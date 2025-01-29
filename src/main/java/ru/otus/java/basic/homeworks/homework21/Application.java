dee1b1f9fb6030f68ff9d1b5eed1f82a86edfcbe
Dmitriy Makarenkov <sitesun707@gmail.com>
2025-01-29 21:10:53 +0300

    Домашнее задание 21 исправлено повторно

--- src/main/java/ru/otus/java/basic/homeworks/homework21/Application.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework21/Application.java
@@ -2,72 +2,64 @@ package ru.otus.java.basic.homeworks.homework21;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
-public class Application {
+class Application {
 
-    private String letter;
+    private class Letter {
+        private String letter = "C";
 
-    public void printLetter(String letter) {
-        System.out.print(letter);
+        public void printLetter(String letter) {
+            this.letter = letter;
+            System.out.print(letter);
+        }
     }
 
-    public void setLetter(String letter) {
-        this.letter = letter;
-    }
+    private final Object lock = new Object();
+    private Letter letterObj = new Letter();
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws InterruptedException {
         Application app = new Application();
-
         ExecutorService executor = Executors.newFixedThreadPool(3);
         executor.execute(()->{
-            synchronized (app) {
-                try {
-                    for (int i = 0; i < 5; i++) {
-                        app.printLetter("A");
-                        app.setLetter("A");
-                        while (!app.letter.equals("C")) {
-                            app.wait();
-                        }
-                        app.notifyAll();
-                    }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
+            app.toA();
         });
         executor.execute(()->{
-            synchronized (app) {
-                try {
-                    for (int i = 0; i < 5; i++) {
-                        while (!app.letter.equals("A")) {
-                            app.wait();
-                        }
-                        app.printLetter("B");
-                        app.setLetter("B");
-                        app.notifyAll();
-                    }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
+            app.toB();
         });
         executor.execute(()->{
-            synchronized (app) {
-                try {
-                    for (int i = 0; i < 5; i++) {
-                        while (!app.letter.equals("B")) {
-                            app.wait();
-                        }
-                        app.printLetter("C");
-                        app.setLetter("C");
-                        app.notifyAll();
-                    }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
+            app.toC();
         });
         executor.shutdown();
+        executor.awaitTermination(1, TimeUnit.SECONDS);
     }
+
+    public void toA() {
+        changeAndShowLetter("C", "A");
+    }
+
+    public void toB() {
+        changeAndShowLetter("A", "B");
+    }
+
+    public void toC() {
+        changeAndShowLetter("B", "C");
+    }
+
+    public void changeAndShowLetter(String currentLetter, String newLetter) {
+        synchronized (lock) {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    while (!letterObj.letter.equals(currentLetter)) {
+                        lock.wait();
+                    }
+                    letterObj.printLetter(newLetter);
+                    lock.notifyAll();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
